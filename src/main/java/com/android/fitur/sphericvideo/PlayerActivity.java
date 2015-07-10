e380fd033ff38c040b6ab7c6a37f5c97a9c83b1c
Sandra Malpica <sandra_malpica@hotmail.com>
2015-07-10 17:50:09 +0200

    initial commit

---
+++ src/main/java/com/android/fitur/sphericvideo/PlayerActivity.java
@@ -0,0 +1,110 @@
+package com.android.fitur.sphericvideo;
+/**
+ * Autor: Sandra Malpica Mallo
+ *
+ * Fecha: 10/07/2015
+ *
+ * Clase: PlayerActivity.java
+ *
+ * Comments: this class displays a 360 video using OpenGL ES 2.0
+ */
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+
+public class PlayerActivity extends Activity {
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //TODO: make a custom view that extends GLSurfaceView to add the video player bar over it
+        //TODO: or use a layout with 2 views in the activity (SLSurface and video bar)
+        super.onCreate(savedInstanceState);
+        /*//full-screen
+        getWindow().setFlags(android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //horizontal orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        final View contentView = findViewById(android.R.id.content);
+        contentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE);*/
+        glSurfaceView = new GLSurfaceView(this);
+        final ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo =
+                activityManager.getDeviceConfigurationInfo();
+
+//        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+        //see if the device supports OpenGL2 (physical and emulator)
+        final boolean supportsEs2 =
+                configurationInfo.reqGlEsVersion >= 0x20000
+                        || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                        && (Build.FINGERPRINT.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("unknown")
+                        || Build.MODEL.contains("google_sdk")
+                        || Build.MODEL.contains("Emulator")
+                        || Build.MODEL.contains("Android SDK built for x86")));
+        //configure the rendering surface
+        if (supportsEs2) {
+            // Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+            // Assign our renderer.
+            glSurfaceView.setRenderer(new OpenGLRenderer(this));
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        setContentView(glSurfaceView);
+//        setContentView(R.layout.activity_player);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_player, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (rendererSet) {
+            glSurfaceView.onPause();
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (rendererSet) {
+            glSurfaceView.onResume();
+        }
+    }
+}
